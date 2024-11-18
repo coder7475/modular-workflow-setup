d67e3eea9c9b84fd08e25439f8a2b036e8dfa5a1
coder7475 <robiulhossain7475@gmail.com>
2024-11-18 18:50:53 +0600

    feat: simulate an app to test logger

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from src.logger import logging
 
 def test_logger():
-    logging.info("Starting the main function")        
+    try:
+        logging.info("Starting the main function")        
 
-    # Simulate some application steps
-    
+        # Simulate some application steps
+        logging.info("Initialize the application")
+        x = 5
+        y = 10
+
+        logging.info(f"Performing {x} + {y}")
+        result = x + y
+
+        logging.info(f"Result: {result}")
+
+        # Simulate a warning
+        if result > 10:
+            logging.warning(f"Result {result} is greater than 10")
+
+    except Exception as e:
+        logging.error("An error occurred in main function", exec_info=True)
+        raise e
+
 
 if __name__ == "__main__":
     logging.info("Starting logger test application")
