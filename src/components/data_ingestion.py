c5331fad5532052e1013d1ef550ea3580e211ac1
coder7475 <robiulhossain7475@gmail.com>
2024-11-21 21:37:50 +0600

    feat: add logging to data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -19,17 +19,20 @@ class DataIngestion:
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
+        logging.info("Data Ingestion started")
         try:
+            logging.info("Data reading using Pandas from local file")
             data = pd.read_csv(os.path.join("data_sources", "adult.csv"))
+            logging.info("End Data Reading")
 
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
             data.to_csv(self.ingestion_config.raw_data_path, index=False)
-
+            logging.info("Data Splitting started")
             train_set, test_set = train_test_split(data, test_size=.20, random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
-
+            logging.info("Data splitting ended")
         except Exception as e:
             logging.info("Error occured in data ingestion stage")
             raise CustomException(e, sys)
