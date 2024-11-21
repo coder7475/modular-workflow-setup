07f61918ded4f12568470ea6b78dc58e8d430e0f
coder7475 <robiulhossain7475@gmail.com>
2024-11-21 21:39:00 +0600

    feat: return the value from data ingestion method

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -33,6 +33,9 @@ class DataIngestion:
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
             logging.info("Data splitting ended")
+
+            return self.ingestion_config.train_data_path, self.ingestion_config.test_data_path
+        
         except Exception as e:
             logging.info("Error occured in data ingestion stage")
             raise CustomException(e, sys)
